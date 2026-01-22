d8fc94b1b0731798fc138095f33c18293c1f01a1
TejasviVermani <vermanitejasvi@gmail.com>
2026-01-22 20:35:38 +0530

    Day 5 - Loops

---
+++ Day 5 - Loops in Python/for_loop.py
@@ -0,0 +1,27 @@
+#for loop on lists
+fruits =['Apple', 'Mango','Pear']
+for fruit in fruits:
+    print(fruit)
+
+#for loop in range
+for i in range(0,10):
+    print(i)
+
+print(fruits)
+
+
+print(range(1, 10))  # Doesn't do anything
+
+for number in range(1, 10):  # Prints 1 to 9
+    print(number)
+
+for number in range(1, 11):  # Prints 1 to 10
+    print(number)
+
+
+# Gauss challenge
+total = 0
+for number in range(1, 101):
+    total += number
+print(total)
+
