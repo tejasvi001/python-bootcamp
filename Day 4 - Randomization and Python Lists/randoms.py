bdd90a1ce7b1432104b7c18a8b723ae91d3f52fd
TejasviVermani <vermanitejasvi@gmail.com>
2026-01-22 15:05:39 +0530

    Day 4 - Randomization and Python Lists

---
+++ Day 4 - Randomization and Python Lists/randoms.py
@@ -0,0 +1,11 @@
+import random
+#randint(a,b) - range - a&b inclusive
+print(random.randint(1,10))
+
+#FLoating Points
+# 0.0<=x<1.0
+x=random.random()*10
+print(x)
+
+#random.uniform(a,b) inclusive
+print(random.uniform(1,10))
---
+++ Day 4 - Randomization and Python Lists/toss.py
@@ -0,0 +1,11 @@
+import random
+x=random.randint(1,2)
+if x==1:
+    print("Heads")
+else:
+    print("Tails")
+
+list=["Heads","Tails"]
+
+x=random.randint(0,1)
+print(list[x])
