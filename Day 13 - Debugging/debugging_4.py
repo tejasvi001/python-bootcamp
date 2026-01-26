1138cdd8dccb90aba2d0335b8d78cbb961664986
TejasviVermani <vermanitejasvi@gmail.com>
2026-01-26 21:08:05 +0530

    Day 13- Debugging

---
+++ Day 13 - Debugging/debugging_4.py
@@ -0,0 +1,5 @@
+age = int(input("How old are you?"))
+if age > 18:
+    print("You can drive at age {age}.")
+else:
+    print("You cant drive")
---
+++ Day 13 - Debugging/describe_the_problem.py
@@ -0,0 +1,12 @@
+def my_function():
+    for i in range(1, 20 ):
+        if i == 20:
+            print("You got it")
+
+
+my_function()
+
+# Describe the Problem - Write your answers as comments:
+# 1. What is the for loop doing?  it will run 19 times
+# 2. When is the function meant to print "You got it"? when i is 20
+# 3. What are your assumptions about the value of i? 1-19
